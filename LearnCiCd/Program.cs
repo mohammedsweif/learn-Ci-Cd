10780db18df1d447b7db944c2f26ab92a26f701e
mohammed sweif <mohamed.sweif@goingInfinity.com>
2025-12-14 19:29:41 +0200

    add bug

--- LearnCiCd/Program.cs
+++ LearnCiCd/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelo pment())
 {
     app.MapOpenApi();
 }
